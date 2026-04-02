d53b5b56345384fd36a7ba00ce9e7212c088f05a
snipeet03 <navneetlonare77@gmail.com>
2026-04-02 08:44:51 +0530

    feat: initialize portfolio project structure with core styling and layout components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import Navbar          from '../components/Navbar'
 import HeroSection     from '../components/HeroSection'
 import AboutSection    from '../components/AboutSection'
+import GithubContributionsSection from '../components/GithubContributionsSection'
 import ProjectsSection from '../components/ProjectsSection'
 import Footer          from '../components/Footer'
 
@@ -12,6 +13,7 @@ export default function Home() {
         <HeroSection />
         <AboutSection />
         <ProjectsSection />
+        <GithubContributionsSection />
       </main>
       <Footer />
     </>
